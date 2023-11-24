ff94d6249fac9ec66a8ec1e30a160a4b2f04184a
Nickgaming8889 <nickgaming8889@gmail.com>
2023-11-24 16:03:33 -0600

    24/11/2023 16:03

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,9 @@ yesBtn.addEventListener('click', function(){
 const noBtn = document.querySelector('#noBtn');
 
 noBtn.addEventListener('mouseover', function(){
-    const randomX = parseInt(Math.random(70)*500);
-    const randomY = parseInt(Math.random(55)*450);
+
+    const randomX = parseInt(Math.random(70)*700);
+    const randomY = parseInt(Math.random(55)*600);
 
     noBtn.style.setProperty('top', randomY+'%');
     noBtn.style.setProperty('left', randomX+'%');
